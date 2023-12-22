fda64e2561332b042d23eefd32f79fedf8605152
bloke yang <39076614+GuoweiYangGitHub@users.noreply.github.com>
2023-12-22 17:18:23 +0800

    Update main.ts
    add elements styles

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import router from './router/index';
 import store from './store';
 import '@/styles/index.scss';
 import 'nprogress/nprogress.css'; // 进度条样式
-
+import 'element-plus/dist/index.css';
 const app = createApp(App);
 
 app.use(router);
